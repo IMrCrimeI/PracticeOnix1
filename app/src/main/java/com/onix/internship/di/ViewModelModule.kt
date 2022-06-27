bd35bbc44507f236ede3fd7fbd22707445075010
IMrCrimeI <imrcrime@gmail.com>
2022-06-27 21:23:53 +0300

    Base code

--- app/src/main/java/com/onix/internship/di/ViewModelModule.kt
+++ app/src/main/java/com/onix/internship/di/ViewModelModule.kt
@@ -1,14 +1,14 @@
 package com.onix.internship.di
 
-import com.onix.internship.ui.canvas.CanvasViewModel
 import com.onix.internship.ui.main.MainViewModel
 import com.onix.internship.ui.splash.SplashViewModel
+import com.onix.internship.ui.translate.TranslateViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { CanvasViewModel() }
     viewModel { SplashViewModel() }
     viewModel { MainViewModel() }
+    viewModel { TranslateViewModel() }
 
 }
